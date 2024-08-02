571e06fe100ad6af7baea4d676c4584f7e9b1e8b
vickywinkel <152075054+vickywinkel@users.noreply.github.com>
2024-08-02 11:55:37 -0300

    Instatiate 1 Objeto

--- Assets/tirarscript.cs
+++ Assets/tirarscript.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class tirarscript : MonoBehaviour
 {
-    public GameObject[] comidas;
-    GameObject comidaAzar; 
+    public GameObject[] comidas; //creamos el array
+    public GameObject comidaAzar;
+
+
+
+    private int Xmix = -3;
+    private int Xmax = 3;
+
+
+    private int Zmix = 4;
+    private int Zmax = 8;
+
+
+
+
+    public Vector3 newPosition; //cordenadas prueba para instanciar 
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < comidas.Length; i++)
+        for (int i = 0; i < comidas.Length; i++) //desactiva todos los objetos del array
         {
-            comidas[i].SetActive(false);
+            comidas[i].SetActive(false); 
         }
+
+
         GenerarRandom();
+
     }
 
 
@@ -27,10 +45,21 @@ public class tirarscript : MonoBehaviour
         
     }
 
-    void GenerarRandom()
+    void GenerarRandom() //funcion para q aparezca un objeto random
     {
         int azar = Random.Range(0, comidas.Length);
         comidaAzar = comidas[azar];
-        GameObject food = Instantiate(comidaAzar); //corregir
+
+
+        float randomX = Random.Range(Xmix, Xmax); // genero random una posicion en X donde quiero que se genere
+        float randomz = Random.Range(Zmix, Zmax);// genero random una posicion en Z donde quiero que se genere
+        newPosition = new Vector3(randomX, 0, randomz); // creo la posicion donde se va a generar con un random X y un random Z
+
+        
+        GameObject clon =  Instantiate(comidaAzar, newPosition, Quaternion.identity); // instanto el prefab
+        clon.SetActive(true);
+
     }
+
+    
 }
